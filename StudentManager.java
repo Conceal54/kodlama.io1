b661875f5cfff575c87c7e18769e3b03e625e51c
Conceal54 <63400713+Conceal54@users.noreply.github.com>
2021-05-10 02:15:47 +0300

    Delete StudentManager.java

--- StudentManager.java
+++
@@ -1,7 +0,0 @@
-package inheritance;
-
-public class StudentManager {
-	public void joinCourse(Student user) {
-		System.out.println("Sayın " + user.name + " " + user.lastname + " Kursa Başayırla Katıldınız.");
-	}
-}
